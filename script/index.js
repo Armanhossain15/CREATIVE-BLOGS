97320769484fe4c8e95fc6da0670858f2fd0bb4b
Armanhossain15 <armanhossain3574@gmail.com>
2024-02-21 12:15:01 +0600

    added upload button

--- script/index.js
+++ script/index.js
@@ -2,3 +2,8 @@ document.getElementById('add-blogs').addEventListener('click', function(){
     hideElementById('blogs')
     showElementById('deshboard')
 })
+
+document.getElementById('btn-upload').addEventListener('click', function(){
+    hideElementById('deshboard')
+    showElementById('blogs')
+})
